be105adcdf8523e6f0dd9ad88d5b5dc55b2ab13f
Tirtadwipa Manunggal <tirtadwipa.manunggal@gmail.com>
2020-04-03 12:51:32 +0700

    Updates

---
+++ precache-manifest.6ca21a351090cc901ce097cd4a7f7594.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "376a20d9a92cb684fc7a40c0066008f9",
+    "url": "/currency-vs-corona/index.html"
+  },
+  {
+    "revision": "190ca79d014c8e83483b",
+    "url": "/currency-vs-corona/static/css/main.e1b9f33a.chunk.css"
+  },
+  {
+    "revision": "3c55dfba7c844374276f",
+    "url": "/currency-vs-corona/static/js/2.dab0eb8b.chunk.js"
+  },
+  {
+    "revision": "88e77cb223281485d18962ec7c3695d9",
+    "url": "/currency-vs-corona/static/js/2.dab0eb8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "190ca79d014c8e83483b",
+    "url": "/currency-vs-corona/static/js/main.e2061e52.chunk.js"
+  },
+  {
+    "revision": "a7e84432e2095d0bcecd",
+    "url": "/currency-vs-corona/static/js/runtime-main.4b316713.js"
+  }
+]);
